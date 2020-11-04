4a26714d9d2f00aae86a25e198e83376bbeb0cb8
Kai Yuan Neo <kai@rocketacademy.co>
2020-11-04 12:32:08 +0800

    fix import extensions

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,11 @@ module.exports = {
     // Don't enforce control flow closing curly brace needs to be
     // on same line as next control flow opening statement
     'brace-style': 'off',
+    // Require import extensions for all imports except packages because
+    // SWE1 doesn't use a bundler for executing Node, thus Node will fail
+    // when importing local modules without the .js extension.
+    // https://github.com/airbnb/javascript/issues/2134
+    'import/extensions': ['error', 'ignorePackages'],
     // Disable linebreak style to prevent ESLint errors on Windows line endings
     // https://eslint.org/docs/rules/linebreak-style
     'linebreak-style': 'off',
